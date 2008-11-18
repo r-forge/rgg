aee01b56fa0d52af8c9e689f76fbddbf816fc022
ilvisne (ilhami visne) <ilhami.visne@ait.ac.at>
2008-11-18 22:25:06 +0000

    pamr and samr added. zip archives and web site have been updated.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgg@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/rgg/branches/rgg-taverna/at/ac/arcs/rgg/element/maimporter/RMAImporter.java
+++ src/rgg/branches/rgg-taverna/at/ac/arcs/rgg/element/maimporter/RMAImporter.java
@@ -69,7 +69,7 @@ public class RMAImporter extends RElement {
                 //channelinfo & colorinfo
                 rbuilder.append(",channel=2,color=\"RG\"");
 
-            } else { //ONECHANNEL
+            } else if(mapanel.getArrayChannelInfo() == ArrayChannelInfo.ONECHANNEL){ //ONECHANNEL
                 if (mapanel.getArrayColorInfo() == ArrayColorInfo.G) {
                     rbuilder.append("G=\"" + mapanel.getGHeader() + "\"");
                     rbuilder.append(", Gb=\"" + mapanel.getGbHeader() + "\"");
@@ -86,6 +86,14 @@ public class RMAImporter extends RElement {
                     //channelinfo & colorinfo
                     rbuilder.append(",channel=1,color=\"R\"");
                 }
+            }else{ //GENERIC TYPE ==> UNKNOWN
+                rbuilder.append("G=\"" + mapanel.getGHeader() + "\"");
+                rbuilder.append(", Gb=\"" + mapanel.getGbHeader() + "\"");
+                rbuilder.append(", R=\"" + mapanel.getRHeader() + "\"");
+                rbuilder.append(", Rb=\"" + mapanel.getRbHeader() + "\")");
+
+                //channelinfo & colorinfo
+                rbuilder.append(",channel=NA,color=NA");
             }
 
             //annotation
